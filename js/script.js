ea4bb15312d625c68b7143144f28799e3c726e98
Swatilekha Roy <swatilekha.roy4@gmial.com>
2021-08-12 17:39:10 +0530

    added font-weight

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ $('#return-to-top').click(function() {
         scrollTop : 0                       
     }, 500);
 });
+
+
+
+/* Form submission without refresh */
+$.ajax({
+    url: "https://formsubmit.co/3877f8bbba51205a28d60d24e448b2d0",
+    method: "POST",
+    data: {
+        name: "FormSubmit",
+        message: "I'm from Devro LABS"
+    },
+    dataType: "json"
+});
